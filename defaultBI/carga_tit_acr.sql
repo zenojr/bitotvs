7471f831d682f4c3ade1c0327087601daf4511f5
zenojr <zfrancajunior@gmail.com>
2019-07-31 10:10:42 -0300

    'updatecarga'

--- defaultBI/carga_tit_acr.sql
+++ defaultBI/carga_tit_acr.sql
@@ -4,3 +4,16 @@ BULK
 INSERT dw_vencimentos
 FROM 'C:\totvsbi\CargaBI\Txt\tit-acr.txt' 
 WITH ( CODEPAGE = 'ACP', FIELDTERMINATOR = ';' , ROWTERMINATOR = '\n' );
+
+update dw_vencimentos
+set dias_vcto = datediff(day, convert(date, dat_vencto_tit_acr, 103), getdate() )
+
+update dw_vencimentos
+set dias_atraso = datediff(day, convert(date, dat_vencto_tit_acr, 103), convert(date, dat_liquidac_tit_acr, 103) )
+
+update dw_vencimentos
+set val_liq_tit_acr = cast( val_liq_tit_acr as decimal(17,2))/100 from dw_vencimentos
+
+update dw_vencimentos
+set val_origin_tit_acr = cast( val_origin_tit_acr as decimal(17,2))/100 from dw_vencimentos
+
